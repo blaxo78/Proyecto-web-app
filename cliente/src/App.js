e7bd54c2374ff836e3c04e6015a2cad34cf1cb52
Bladimir <blarock7@gmail.com>
2022-11-10 16:08:44 -0500

    se crearon archivos .js de los items

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>APLICACION WEB GRUPO E </h1>
+      <h2>Ciclo 4 - 2022</h2>
     </div>
   );
 }
